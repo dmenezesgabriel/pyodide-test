25dea052def738e64ca63cc972a4dd6ce423cb43
Gabriel Menezes <dmenezes.gabriel@gmail.com>
2021-11-29 09:10:09 -0300

    Iterate and add values to document

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var content = null;
+var url = null;
 
 function sample() {
   var file = document.getElementById("file").files[0];
@@ -6,7 +7,10 @@ function sample() {
   reader.readAsText(file, "UTF-8");
   reader.onload = (event) => {
     content = event.target.result;
-
+    var blob = new Blob([content], { type: "text/plain" });
+    url = window.URL.createObjectURL(blob);
+    var a = document.createElement("a");
+    a.href = url;
     fetch("/file_test.py").then((response) => {
       return response.text().then((text) => {
         pyodide.runPythonAsync(text);
